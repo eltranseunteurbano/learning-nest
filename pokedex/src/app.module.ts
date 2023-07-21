599024e5f79055f5fc671fcfc2978e18790ad8b0
Jaime Burbano <burbanojaime98@gmail.com>
2023-07-21 10:40:42 -0500

    wip: other pc

--- pokedex/src/app.module.ts
+++ pokedex/src/app.module.ts
@@ -1,18 +1,22 @@
 import { join } from 'path';
+
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
       renderPath: '/',
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     PokemonModule,
     CommonModule,
     SeedModule,
