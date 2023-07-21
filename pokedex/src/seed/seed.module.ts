c44a2ad8034975f54de52c5039a2634c379844c8
Jaime Burbano <burbanojaime98@gmail.com>
2023-07-20 19:16:31 -0500

    custom http provider

--- pokedex/src/seed/seed.module.ts
+++ pokedex/src/seed/seed.module.ts
@@ -3,10 +3,11 @@ import { PokemonModule } from 'src/pokemon/pokemon.module';
 
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
-  imports: [PokemonModule],
+  imports: [PokemonModule, CommonModule],
 })
 export class SeedModule {}
--- pokedex/src/seed/seed.service.ts
+++ pokedex/src/seed/seed.service.ts
@@ -4,20 +4,23 @@ import { PokeResponse } from './interfaces/poke-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapter/axios.adapter';
 
 @Injectable()
 export class SeedService {
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
 
   private readonly axios: AxiosInstance = axios;
 
   async executeSeed() {
     await this.pokemonModel.deleteMany({});
-    const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=900',
+
+    const data = await this.http.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=9',
     );
 
     const pokemons = data.results.map(({ name, url }) => {
@@ -25,12 +28,6 @@ export class SeedService {
       const no = +segments[segments.length - 2];
       return { name, no };
     });
-
-    // data.results.forEach(async ({ name, url }) => {
-    //   const segments = url.split('/');
-    //   const no = +segments[segments.length - 2];
-    //   await this.pokemonModel.create({ name, no });
-    // });
 
     const pokemonsInserted = await this.pokemonModel.insertMany(pokemons);
 
